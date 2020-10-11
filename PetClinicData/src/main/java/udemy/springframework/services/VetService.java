1d43e83d09c198382672bef230835d5559462a57
Pasha <fshrf158@gmail.com>
2020-10-11 21:42:01 +0300

    pet_clinic

---
+++ PetClinicData/src/main/java/udemy/springframework/services/VetService.java
@@ -0,0 +1,14 @@
+package udemy.springframework.services;
+
+import udemy.springframework.model.petclinic.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
