a679c27d6a7017e7aee5b6a2a4acf313d0a989f7
Pasha <fshrf158@gmail.com>
2020-10-25 22:40:17 +0200

    Adding index page and controller

---
+++ PetClinicWeb/src/main/java/udemy/springframework/model/petclinic/Controllers/IndexController.java
@@ -0,0 +1,13 @@
+package udemy.springframework.model.petclinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index.html"})
+    public String index() {
+        return "index";
+    }
+}
