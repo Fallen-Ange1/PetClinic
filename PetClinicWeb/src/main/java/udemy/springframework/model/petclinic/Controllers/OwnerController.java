04c9ee080279976a6f92aaeb167123ddea9b8b49
Pasha <fshrf158@gmail.com>
2020-10-25 22:59:27 +0200

    Adding owner page and  controller

---
+++ PetClinicWeb/src/main/java/udemy/springframework/model/petclinic/Controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package udemy.springframework.model.petclinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/owners"})
+public class OwnerController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String listOwner(){
+        return "owners/index";
+    }
+}
