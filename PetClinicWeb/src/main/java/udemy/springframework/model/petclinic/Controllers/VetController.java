d48faa025d60220f20b083bf887f85600f7e54c7
Pasha <fshrf158@gmail.com>
2020-11-20 15:31:52 +0200

    Adding Vet List and updated Controller.

--- PetClinicWeb/src/main/java/udemy/springframework/model/petclinic/Controllers/VetController.java
+++ PetClinicWeb/src/main/java/udemy/springframework/model/petclinic/Controllers/VetController.java
@@ -1,14 +1,23 @@
 package udemy.springframework.model.petclinic.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import udemy.springframework.model.petclinic.services.VetService;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
